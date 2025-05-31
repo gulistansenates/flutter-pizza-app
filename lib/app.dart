4792a5cfe35f40096e29e5d6d6f2bcf3bf2f3ac3
gulistansenates <gulistansenates@gmail.com>
2025-05-31 15:31:59 +0300

    Set up Firebase initialization and BLoC observer with user repository injection

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:user_repository/src/firebase_user_repo.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp(FirebaseUserRepo firebaseUserRepo, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
