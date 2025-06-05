02444aba2e9cadf85a47644d2947be68441333ff
gulistansenates <gulistansenates@gmail.com>
2025-06-05 22:25:15 +0300

    Add new widgets and enhancements to home screen UI

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
-import 'package:pizza_app/screens/auth/views/welcome_screen.dart';
-import 'package:pizza_app/screens/home/views/home_screen.dart';
+import 'package:pizza_app/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+
+import 'screens/auth/views/welcome_screen.dart';
+import 'screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -13,16 +15,19 @@ class MyAppView extends StatelessWidget {
       title: 'Pizza Delivery',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.light(
-            surface: Colors.grey.shade200,
-            onSurface: Colors.black,
-            primary: Colors.blue,
-            onPrimary: Colors.white),
-      ),
+          colorScheme: ColorScheme.light(
+              surface: Colors.grey.shade200,
+              onSurface: Colors.black,
+              primary: Colors.blue,
+              onPrimary: Colors.white)),
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: ((context, state) {
           if (state.status == AuthenticationStatus.authenticated) {
-            return const HomeScreen();
+            return BlocProvider(
+              create: (context) =>
+                  SignInBloc(context.read<AuthenticationBloc>().userRepository),
+              child: const HomeScreen(),
+            );
           } else {
             return const WelcomeScreen();
           }
